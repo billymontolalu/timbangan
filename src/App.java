c1fd59ff944e1285c612b12dee715623bfdd415e
Billy Montolalu <billy.montolalu@gmail.com>
2021-07-28 15:26:46 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,139 @@
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class App
+{
+    public App()
+    {
+        super();
+    }
+    
+    void connect ( String portName ) throws Exception
+    {
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if ( portIdentifier.isCurrentlyOwned() )
+        {
+            System.out.println("Error: Port is currently in use");
+        }
+        else
+        {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+            
+            if ( commPort instanceof SerialPort )
+            {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(2400,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+                
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+                
+                (new Thread(new SerialReader(in))).start();
+
+            }
+            else
+            {
+                System.out.println("Error: Only serial ports are handled by this example.");
+            }
+        }     
+    }
+    
+    /** */
+    public static class SerialReader implements Runnable 
+    {
+        InputStream in;
+        
+        public SerialReader ( InputStream in )
+        {
+            this.in = in;
+        }
+        
+        public void run ()
+        {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            try
+            {
+                while ( ( len = this.in.read(buffer)) > -1 )
+                {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    String temp = new String(buffer,0,len);
+                    String splitdata[] = temp.split("=");
+                    
+                    if(splitdata.length > 3)
+                    {
+                        //System.out.print(splitdata[4]);
+                        String splitdata2[] = splitdata[4].trim().split("");
+                        int counter = 0;
+                        String weight = "";
+                        for(int i=splitdata2.length-1; i>=0; i--){
+                            if(counter >= 4){
+                                weight = weight + splitdata2[i];
+                            }
+                            counter++;
+                            
+                        }
+                        System.out.println(weight);
+                    }
+                    
+
+                }
+                System.out.println("test");
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }            
+        }
+    }
+
+    /** */
+    public static class SerialWriter implements Runnable 
+    {
+        OutputStream out;
+        
+        public SerialWriter ( OutputStream out )
+        {
+            this.out = out;
+        }
+        
+        public void run ()
+        {
+            try
+            {                
+                int c = 0;
+                while ( ( c = System.in.read()) > -1 )
+                {
+                    this.out.write(c);
+                }                
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }            
+        }
+    }
+    
+    public static void main ( String[] args )
+    {
+        try
+        {
+            (new App()).connect("COM4");
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
